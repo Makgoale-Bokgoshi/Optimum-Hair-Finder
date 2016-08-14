ac9af907053f08aa91e33dea0a670b9fd03b0dd6
sefalab <bonjosefala@gmail.com>
2016-08-14 16:48:13 +0200

    Update UserLogin.php

--- php scripts/UserLogin.php
+++ php scripts/UserLogin.php
@@ -1,29 +1,29 @@
 <?php     //start php tag
+error_reporting(E_ALL); ini_set('display_errors', 1);
 //include connect.php page for database connection
 Include('connect.php');
 //if submit is not blanked i.e. it is clicked.
 
-//If($_REQUEST['email']=='' || $_REQUEST['password']=='')//
-//{
-//Echo "Please fill in all fields.";
-//}
-//Else
-//{
-$sql=" select User_Full_Name from all_users where User_Email='laura.magoshi@gmail.com' and User_Password ='aa' " ;
-$res=mysql_query($sql);
-If($res)
+
+if(isset($_POST['submit']))
 {
-Echo "Welcome ";
-while($row = mysql_fetch_assoc($res)){
-	foreach($row as $cname=>$cvalue){
-		print "$cvalue";
+	$query = mysqli_query($con,"SELECT *  FROM all_users where User_Email = '$_POST[email]' AND User_Password = '$_POST[password]'") or die(mysql_error());
+	$row = mysqli_fetch_array($query);
+	if(!empty($row['User_Email']) AND !empty($row['User_Password']))
+	{
+		$_SESSION['User_Email'] = $row['User_Password'];
+		echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE...";
+
+	}
+	else
+	{
+		echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY...";
 	}
 }
-}
-Else
-{
-Echo "Error in connection";
 
-//}
-}
+
+
+
+
+
 ?>
