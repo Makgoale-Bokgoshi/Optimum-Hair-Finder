02024b03013f55113e9342a7eb586d9e3b48921c
sefalab <bonjosefala@gmail.com>
2016-10-31 19:14:32 +0200

    Update call.php

--- New_System/call.php
+++ New_System/call.php
@@ -1,10 +1,104 @@
 <?php
+	include 'define.php';
+	include 'functions.php';
+	session_start();
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Home</title><link rel='shortcut icon' href='favicon.ico'/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<!-- top-header -->
+<div class="top-header">
+	<div class="container">
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 			
+			<li class="sig"><a href="#"  ><?php 
+			include 'define.php';
+			
+			$name = $_SESSION['res'];
+			
+			echo $name;?></a></li> 
+			<li class="sigi"><a href="index01.php" >  Sign out</a></li>
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /top-header ---->
+<!--- footer-btm ---->
+<div class="footer-btm wow fadeInLeft animated" data-wow-delay=".5s">
+		<div class="container">
+				<div class="navigation">
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+						<div style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;" class="logo wow fadeInDown animated animated" data-wow-delay=".5s">
+			<a href="index01.html"><img src="images/logo.jpg" class="img-responsive" alt=""></a>	
+		</div>
+						  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						  </button>
+						</div>
+					</nav>
+				</div>
+		
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /footer-btm ---->
+
+
+<?php
+
 	include_once 'functions.php';
 
 	$h = $_POST['hairstyle'];
 	$mi = $_POST['minprice'];
 	$ma = $_POST['maxprice'];
-	//echo $min;
-	$l = $_POST['location'];
-	search($h, $l, $mi, $ma);
+	$lo = $_POST['location'];
+	search($h, $lo, $mi, $ma);
+
 ?>
+<div class="clearfix"></div></div>
+	<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="#"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="#"><span>Twitter</span></a></li>
+				
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">2016 Optimum hair finder . All Rights Reserved</p>
+	</div>
+</div>
+<!---copy-right ---->
+</body>
+
+</html>
