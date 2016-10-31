ce9a7384b530f2244d89ab9f8fe22e98c69b52f2
sefalab <bonjosefala@gmail.com>
2016-10-31 07:16:38 +0200

    Update Dreadlocks.php

--- New_System/Dreadlocks.php
+++ New_System/Dreadlocks.php
@@ -1,51 +1,101 @@
-<<<<<<< HEAD:New_System/Dreadlocks.php
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Gallery</title><link rel='shortcut icon' href='favicon.ico'/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/modal.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script src="js/modal.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
 
 
-	<html>
-	<body> 
-	<?php 
-	$mysqlserver ="localhost";
-$mysqlusername ="root";
-$mysqlpassword ='';
-$dbname = "hair_studios";
-$con=mysqli_connect($mysqlserver,$mysqlusername,$mysqlpassword) or die("Failed to connect to MySQL: " . mysqli_error());
-$db=mysqli_select_db($con,$dbname) or die("Failed to connect to MySQL: " . mysqli_error());
+<script>
+    var expanded = false;
+    function showCheckboxes() {
+        var checkboxes = document.getElementById("checkboxes");
+        if (!expanded) {
+            checkboxes.style.display = "block";
+            expanded = true;
+        } else {
+            checkboxes.style.display = "none";
+            expanded = false;
+        }
+    }
+</script>
+</head>
+<body>
 
 
-    $img_url = "http://www.domain.com/images/"; 
+<div class="agent">
+<div class="ag-bt"><h3 style="text-align: -moz-center;">Dreadlocks</h3> </div>
+	<div class="container"style="/*! border: blue; */border: 1px solid rgba(183, 209, 224, 0.6);">
+	
+<?php
+
+include 'define.php';
     $result = mysqli_query($con,"SELECT distinct Hairstyle_Picture FROM pricing where Style_ID IN (SELECT Style_ID from hair_styles where Category_ID = 5)");
 	///*where Category_ID=1*/
     while($row = mysqli_fetch_assoc($result)) 
     { 
-echo '<img src="./Style_Pictures/Dreadlocks/'.$row["Hairstyle_Picture"]. '" width="225" height="200"  style="margin-right: 5px "/>';
+echo '<a href="#" id="link1" data-toggle="modal" data-target="#myModal4"><img src="./Style_Pictures/Dreadlocks/'.$row["Hairstyle_Picture"]. '" width="225" height="200"  style="margin-right: 5px "/></a>';
         
     }
 	?>
+</div>
+	</div>
+	
+	             
+  </div>  
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title" style="text-align: center;"> Dreadlocks </h4>
+        </div>
+        <div class="modal-body" id="showImg">
+          <!-- here we create the image dynamically -->
+        </div>
+        <div class="modal-footer">
+		<a href="#" data-toggle="modal" data-target="#myModal4" >View details</a>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<script>
+$(document).ready(function() {
+    $('img').on('click', function() {
+        $("#showImg").empty();
+        var image = $(this).attr("src");
+        $("#showImg").append("<img class='img-responsive' src='" + image + "' />")
+    })
+});
+
+
+</script>
+	
 	</body>
 	</html>
-=======
 
-
-	<html>
-	<body> 
-	<?php 
-	$mysqlserver ="localhost";
-$mysqlusername ="root";
-$mysqlpassword ='';
-$dbname = "hair_studios";
-$con=mysqli_connect($mysqlserver,$mysqlusername,$mysqlpassword) or die("Failed to connect to MySQL: " . mysqli_error());
-$db=mysqli_select_db($con,$dbname) or die("Failed to connect to MySQL: " . mysqli_error());
-
-
-    $img_url = "http://www.domain.com/images/"; 
-    $result = mysqli_query($con,"SELECT distinct Hairstyle_Picture FROM pricing where Style_ID IN (SELECT Style_ID from hair_styles where Category_ID = 5)");
-	///*where Category_ID=1*/
-    while($row = mysqli_fetch_assoc($result)) 
-    { 
-echo '<a href="#" data-toggle="modal" data-target="#myModal4" ><img src="./Style_Pictures/Dreadlocks/'.$row["Hairstyle_Picture"]. '" width="225" height="200"  style="margin-right: 5px "/></a>';
-        
-    }
-	?>
-	</body>
-	</html>
->>>>>>> c1ac01a71d62aa50b6322fe9587488a1c19ee2cd:The%2520UI/Dreadlocks.php
